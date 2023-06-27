bc5ab0b71666ca42a1fcbfc4f3b43b76c6b33599
Sanjay <sanjuchopracool@gmail.com>
2023-06-27 21:09:47 +0530

    updated ubunut buiuld file

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -6,27 +6,40 @@ echo  PWD= $PWD
 echo  HOME= $HOME
 echo  USER= $USER
 
-pushd $HOME
+pushd ${HOME}
+
+#check if GOOGLE exist in home directory
+GOOGLE_DIR=${HOME}/GOOGLE
+
+if [ -d "${GOOGLE_DIR}" ];
+then
+    echo "${GOOGLE_DIR} directory exists"
+else
+    echo "${GOOGLE_DIR} directory does not exist. creating it"
+    mkdir -p ${GOOGLE_DIR}
+fi
+
+cd ${GOOGLE_DIR}
 
 # check if the depot_tools exist
-DEPOT_TOOLS_DIR=$HOME/depot_tools
-if [ -d "$DEPOT_TOOLS_DIR" ];
+DEPOT_TOOLS_DIR=${GOOGLE_DIR}/depot_tools
+if [ -d "${DEPOT_TOOLS_DIR}" ];
 then
-    echo "$DEPOT_TOOLS_DIR directory exists. skipping depot_tools installation"
+    echo "${DEPOT_TOOLS_DIR} directory exists. skipping depot_tools installation"
 else
-    echo "$DEPOT_TOOLS_DIR directory does not exist. cloning depot_tools"
+    echo "${DEPOT_TOOLS_DIR} directory does not exist. cloning depot_tools"
     git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
 fi
 
 export PATH="${DEPOT_TOOLS_DIR}:${PATH}"
-echo PATH=$PATH
+echo PATH=${PATH}
 
-SKIA_DIR=$HOME/skia
-if [ -d "$SKIA_DIR" ];
+SKIA_DIR=${GOOGLE_DIR}/skia
+if [ -d "${SKIA_DIR}" ];
 then
-    echo "$SKIA_DIR directory exists. skipping skia fetch"
+    echo "${SKIA_DIR} directory exists. skipping skia fetch"
 else
-    echo "$SKIA_DIR directory does not exist. fetching skia"
+    echo "${SKIA_DIR} directory does not exist. fetching skia"
     fetch skia
     cd ${SKIA_DIR}
     python3 tools/git-sync-deps
