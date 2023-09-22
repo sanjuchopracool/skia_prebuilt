4b665f3faacbdd17bd5dc571e4a43a6a3eeee9a3
Sanjay <sanjuchopracool@gmail.com>
2023-09-22 14:26:41 +0530

    added skia

--- scripts/main.py
+++ scripts/main.py
@@ -111,11 +111,11 @@ def clone_skia():
     cmd = ["git", "pull"]
     run_cmd(cmd)
     cmd = ["python3", "tools/git-sync-deps"]
-    if run_cmd(cmd, True):
+    if run_cmd(cmd):
         run_cmd["python", "tools/git-sync-deps"]
     if K_IS_WINDOWS:
         cmd = ["python3", "bin/fetch-ninja"]
-        if run_cmd(cmd, True):
+        if run_cmd(cmd):
             cmd = ["python", "bin/fetch-ninja"]
     else:
         run_cmd(["bin/fetch-ninja"])
