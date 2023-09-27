d095da858f79d0bf7aed01fe79621f91f1bc5a44
Sanjay <sanjuchopracool@gmail.com>
2023-09-27 17:27:58 +0530

    check for windows

--- scripts/main.py
+++ scripts/main.py
@@ -122,12 +122,15 @@ def clone_skia():
     try :
         inShell =  K_IS_WINDOWS
         cmd = ["python3", "tools/git-sync-deps"]
-        if run_cmd(cmd, inShell):
-            run_cmd(["python3", "tools/git-sync-deps"], inShell)
+        
         if K_IS_WINDOWS:
             os.environ["PATH"] = K_SKIA_PATH + "/third_party/externals/emsdk" + os.pathsep + os.environ["PATH"]
             os.environ["PATH"] = K_SKIA_PATH + "/third_party/externals/emsdk/node/16.20.0_64bit/bin" + os.pathsep + os.environ["PATH"]
             os.environ["PATH"] = K_SKIA_PATH + "/third_party/externals/emsdk/upstream/emscripten" + os.pathsep + os.environ["PATH"]
+
+        if run_cmd(cmd, inShell):
+            run_cmd(["python3", "tools/git-sync-deps"], inShell)
+        if K_IS_WINDOWS:
             cmd = ["python3", "bin/fetch-ninja"]
             if run_cmd(cmd):
                 cmd = ["python3", "bin/fetch-ninja"]
